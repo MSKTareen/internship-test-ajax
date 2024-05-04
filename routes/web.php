7cf4da5c5f217133c5c0945b4c3faff904e012ca
MSKTareen <mohammad.shoaib.khan.tareen.mskt@gmail.com>
2024-05-04 11:35:18 +0500

    Revert "First Project on Github"
    This reverts commit 980179b8f554574a5284dc6a81c99984a3a6237e.

--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-use App\Http\Controllers\TaskController;
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('',[TaskController::class, 'index']);
-Route::post('save',[TaskController::class, 'savedata']);
-Route::get('edit/{id}',[TaskController::class, 'editdata']);
-Route::get('delete/{id}',[TaskController::class, 'delete']);
